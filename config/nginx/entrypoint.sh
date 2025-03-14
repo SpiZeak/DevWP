a35b2714adc1d1d9e2331552ca41ad754022f743
SpiZeak <max@trewhitt.se>
2025-03-14 14:47:42 +0100

    Remove Nginx entrypoint script; implement dynamic Nginx config generation and index.html creation for new sites

--- config/nginx/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-chown -R 1000:1000 /src/www
-chown 1000:1000 /usr/bin/docker-entrypoint.sh
-chmod +x /usr/bin/docker-entrypoint.sh
-
-# Execute the original entrypoint script with passed arguments
-exec /usr/bin/docker-entrypoint.sh "$@"
