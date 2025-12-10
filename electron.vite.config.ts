13d87afae5c662be774937f8af21f020879129d6
SpiZeak <max@trewhitt.se>
2025-12-10 14:55:24 +0100

    refactor: remove redundant import and clean up code

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,8 +1,7 @@
-// @ts-expect-error: https://github.com/tailwindlabs/tailwindcss/discussions/16250
+import { resolve } from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'electron-vite';
-import { resolve } from 'path';
 
 export default defineConfig({
   main: {},
